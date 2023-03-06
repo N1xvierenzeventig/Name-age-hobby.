229f26e88a81f37325486b0cb5db83d55dbdd1de
N1xvierenzeventig <111287243+N1xvierenzeventig@users.noreply.github.com>
2023-03-06 17:04:46 +0100

    Add files via upload

---
+++ Random name +Iq.py
@@ -0,0 +1,16 @@
+name = input("What's your name?")
+age = input("What's your age?")
+hobby = input("What's your favorite hobby?")
+
+
+
+def qwerty(name, age, hobby):
+    print("Your name is " + name + " and your age is " +str(age) + " and as I assume your hobby is " +hobby + ".")
+
+qwerty(name, age, hobby)
+
+
+
+
+
+
